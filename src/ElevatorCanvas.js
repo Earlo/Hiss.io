6c4bd014bc9f69dc8eaa1d455de0d95bcabd4e73
Riku <riku@neverthink.tv>
2017-11-24 21:56:53 +0200

    add ref to canvas

--- src/ElevatorCanvas.js
+++ src/ElevatorCanvas.js
@@ -4,7 +4,7 @@ import Controls from './Controls'
 export default class ElevatorCanvas extends Component {
   render () {
     return <div>
-      <canvas id="hissi-canvas" width="600" height="600"/>
+      <canvas ref={canvas => this.canvas = canvas } id="hissi-canvas" width="600" height="600"/>
       <Controls moveUp={this.moveUp} moveDown={this.moveDown}/>
     </div>
   }
