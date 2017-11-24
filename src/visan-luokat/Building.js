fff61f6ae921938dd2e884d102e52c84140f1a41
Riku <riku@neverthink.tv>
2017-11-24 23:39:30 +0200

    draw floors, building and elevator chute

--- src/visan-luokat/Building.js
+++ src/visan-luokat/Building.js
@@ -4,6 +4,8 @@ export default class Building {
   constructor(levels) {
     this.levels = levels
     this.levelHeight = 32
+    this.buildingHeight = this.levels * this.levelHeight
+    this.buildingWidth = 500
     this.elevators = [new Elevator(0, 0) ]
     this.elevators[0].goingTo = 4
     this.abajs = []
--- src/visan-luokat/Drawer.js
+++ src/visan-luokat/Drawer.js
@@ -17,6 +17,9 @@ export default class Drawer {
     this.building.update()
     //TODO set variable
     this.context.clearRect(0,0,600,600)
+    this.drawBuilding()
+    this.drawElevatorChute()
+    this.drawFloors()
     this.building.elevators.forEach((elevator) => {
         console.log(this.building.levelHeight)
         this.drawElevator(this.height - elevator.getGraphicalHeight() * this.building.levelHeight)
@@ -35,4 +38,36 @@ export default class Drawer {
     ///DO
   }
 
+  drawBuilding(){
+    const height = this.building.buildingHeight
+    const width = this.building.buildingWidth
+    this.context.strokeStyle = "#FF0000"
+    this.context.strokeRect(20, 600 - height,width,height);
+    this.context.stroke();
+  }
+
+  drawElevatorChute() {
+    const width = 24
+    const height = this.building.buildingHeight
+    this.context.strokeStyle = "#000"
+    this.context.strokeRect(21, 600 - height,width,height)
+    this.context.stroke();
+  }
+
+  drawFloors() {
+    const floorCount = this.building.levels
+    const floorHeight = this.building.levelHeight
+    const startX = 45
+    const endX = 45 + this.building.buildingWidth - 25
+    let y
+    const { context } = this
+    for(let i = 1; i < floorCount; i++){
+      y = 600 - i * floorHeight
+      context.beginPath()
+      context.moveTo(startX, y)
+      context.lineTo(endX, y)
+      this.context.strokeStyle = "#000"
+      context.stroke()
+    }
+  }
 }
