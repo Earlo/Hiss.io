00ba284ab056045d5bd570262913fad94a6d9b1c
Riku <riku@neverthink.tv>
2017-11-24 21:14:47 +0200

    fix css

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ElevatorCanvas from './ElevatorCanvas'
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div id="game">
         <ElevatorCanvas/>
         <Controls/>
       </div>
