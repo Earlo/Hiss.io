f4062a27b814507db5807e1153ff3983cf502a59
Riku <riku@neverthink.tv>
2017-11-24 23:09:58 +0200

    remove unused serviceworker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/styles.css'
 import "./styles/font-awesome-4.7.0/css/font-awesome.min.css"
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
