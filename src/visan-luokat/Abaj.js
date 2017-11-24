04dd297b972ec5ff3ed02b0523e2efc5ea90b4ec
Riku <riku@neverthink.tv>
2017-11-25 00:45:47 +0200

    wip on abaj logic

--- src/visan-luokat/Abaj.js
+++ src/visan-luokat/Abaj.js
@@ -1,12 +1,23 @@
 export default class Abaj {
-  constructor(level, position) {
+  constructor(level, position, destination) {
     this.level = level
     this.position = position
+    this.destination = destination
   }
 
   move() {
-    if(this.position > 30){
-      this.position -= 1
+    if(this.level !== this.destination){
+      this.moveTowardsElevator()
+    }else{
+      this.moveTowardsExit()
     }
+  }
+
+  moveTowardsElevator(){
+    console.log('DO ME')
+  }
+
+  moveTowardsExit(){
+    console.log('DO ME')
   }
 }
